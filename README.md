cef6a872a0994eb3a22110fed2e58bb817f0ed3d
Chun-Yang <yang2007chun@gmail.com>
2017-12-09 09:57:45 -0600

    change

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # config
 A index for all of my configs
 
+## install
+curl https://raw.githubusercontent.com/Chun-Yang/config/master/install.sh | sh
+
+## tools
 - [vim](https://github.com/Chun-Yang/vim-config)
 - [zsh](https://github.com/Chun-Yang/zsh-config)
 - [tmux](https://github.com/Chun-Yang/tmux-config)
---
+++ install.sh
@@ -0,0 +1,4 @@
+curl https://raw.githubusercontent.com/Chun-Yang/vim-config/master/install.sh | sh
+curl https://raw.githubusercontent.com/Chun-Yang/zsh-config/master/install.sh | sh
+curl https://raw.githubusercontent.com/Chun-Yang/tmux-config/master/install.sh | sh
+curl https://raw.githubusercontent.com/Chun-Yang/karabiner-config/master/install.sh | sh
